682b91298ee5bdcf259bc5a3b5f9d15dc4aeff2d
Enoch Yu <71986194+YuEnoch@users.noreply.github.com>
2023-02-20 03:05:18 -0500

    Update DESEQtoFASTA.py

--- DESEQtoFASTA.py
+++ DESEQtoFASTA.py
@@ -1,12 +1,10 @@
-#YuEnoch 13-11-2022
+#YuEnoch 20-02-2023
 #DESEQtoFASTA.py 
 #Based off code from Dr. Kart Tomberg -> https://github.com/tombergk/NNK_VWF73/
 
 #Purpose: converts the results from DESeq2 into Fasta files
 
-
 import sys
-
 number = sys.argv[1]
 fileName = sys.argv[2]
 input = open(fileName, 'r')
